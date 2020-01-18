b0b08d108baea23c8a855a7cde045544b7fd6862
Pavel <smartflaber@gmail.com>
2020-01-18 14:24:12 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        navText: ('',''),
+        loop: true
+    });
+});
